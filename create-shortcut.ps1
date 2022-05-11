21248d15437c0509350c8380902cd9d372025082
GTS-Corne <55291082+cornevdh@users.noreply.github.com>
2022-05-11 16:46:50 +0200

    Add files via upload

---
+++ create-shortcut.ps1
@@ -0,0 +1,7 @@
+﻿$SourceFilePath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+$ShortcutPath = "C:\Users\User\Desktop\powershell.lnk"
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
+$shortcut.TargetPath = $SourceFilePath
+$shortcut.WindowStyle = 1
+$shortcut.Save()
